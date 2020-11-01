7e45f58bc9ebfaba8ae3ccf496d134e71e866a95
Ebrish <66562433+ebrish3000@users.noreply.github.com>
2020-11-01 08:43:17 -0800

    got man branch 3rd comment

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,5 +2,6 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("batman changed in got man branch");
         // add one line to commit
+        // add another commit
     }
 }

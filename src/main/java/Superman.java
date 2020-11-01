563368e2ec376fcb3fd2d4117c848972e2b13ebf
Ebrish <66562433+ebrish3000@users.noreply.github.com>
2020-11-01 08:52:59 -0800

    commit on star branch

--- src/main/java/Superman.java
+++ src/main/java/Superman.java
@@ -1,5 +1,7 @@
 public class Superman {
     public static void main(String[] args) {
+
         System.out.println("superman changed");
+        System.out.println("superman changed2 in star branch");
     }
 }
